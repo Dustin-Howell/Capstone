e52702ddd7d7bce2b800ce38658302b64adab232
Dustin Howell <dustin.l.howell@gmail.com>
2013-01-25 12:21:07 -0600

    ProtoCreeper Nearing completion

---
+++ Prototype/ProtoCreeper/ITile.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProtoCreeper
+{
+    public interface ITile
+    {
+        Color Color { get; set; }
+        bool HasTile { get; }
+    }
+
+    public class ProtoTile : ITile
+    {
+        public Color Color { get; set; }
+        public bool HasTile { get { return Color != Color.Empty; } }
+
+        public ProtoTile(Color color = Color.Empty)
+        {
+            Color = color;
+        }
+    }
+}
--- Prototype/ProtoCreeper/Program.cs
+++ Prototype/ProtoCreeper/Program.cs
@@ -8,8 +8,29 @@ namespace ProtoCreeper
 
     class Program
     {
+        //TODO: Write a real main that lets us play a game of creeper on the computer
         static void Main(string[] args)
         {
+            CreeperBoard board = new CreeperBoard();
+            board.PrintToConsole();
+
+            int startRow, startCol, endRow, endCol;
+
+            Console.Write("Start Row: ");
+            startRow = Int32.Parse(Console.ReadLine());
+
+            Console.Write("Start Col: ");
+            startCol = Int32.Parse(Console.ReadLine());
+
+            Console.Write("End Row: ");
+            endRow = Int32.Parse(Console.ReadLine());
+
+            Console.Write("End Col: ");
+            endCol = Int32.Parse(Console.ReadLine());
+
+            board.Move(startRow, startCol, endRow, endCol);
+
+            board.PrintToConsole();
         }
     }
 }
