c5a9104adfd7ea6aeeb512fa249a52b64e2dbd45
Dustin Howell <Dustin.l.howell@gmail.com>
2013-01-28 17:26:41 -0600

    Added AI project

---
+++ Fire and Ice/CreeperAI/CreeperAI.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CreeperAI
+{
+    public class CreeperAI
+    {
+        public Point GetMove(int[] pegs, int[] tiles)
+        {
+
+
+            return new Point(0, 0);
+        }
+    }
+}
