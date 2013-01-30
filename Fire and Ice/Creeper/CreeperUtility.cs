ff19e41f28a96717b20a4c5dff925b8f35a344a3
Dustin Howell <dustin.l.howell@gmail.com>
2013-01-30 15:02:55 -0600

    Removal of position class from CreeperUtility.cs

--- Fire and Ice/Creeper/CreeperUtility.cs
+++ Fire and Ice/Creeper/CreeperUtility.cs
@@ -5,24 +5,6 @@ using System.Text;
 
 namespace Creeper
 {
-    public class Position
-    {
-        public Position() { }
-
-        public Position(int row, int col)
-        {
-            Column = col;
-            Row = row;
-        }
-
-        public int Column { get; set; }
-        public int Row { get; set; }
-
-        public bool Equals(Position position)
-        {
-            return (Column == position.Column && Row == position.Row);
-        }
-    }
 
     public static class CreeperUtility
     {
