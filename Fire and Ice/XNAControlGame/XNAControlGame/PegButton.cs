55a8795be58b8c64fc8abce0e985ad73fd5b2bab
Dustin Howell <dustin.l.howell@gmail.com>
2013-01-30 12:43:11 -0600

    Added clickable and children

---
+++ Fire and Ice/XNAControlGame/XNAControlGame/PegButton.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Creeper;
+using Microsoft.Xna.Framework;
+
+namespace XNAControlGame
+{
+    public class PegButton : Button
+    {
+        public CreeperColor Color { get; set; }
+
+        public PegButton(Game1 game, Vector2 position, Vector2 size, OnClickDelegate onClick) : base(game, position, size, onClick, "")
+        {
+
+        }
+    }
+}
