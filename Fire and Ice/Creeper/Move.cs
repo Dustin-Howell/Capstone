062c88513fd6169509a01b3620a82ed1d0105e80
Dustin Howell <dustin.l.howell@gmail.com>
2013-01-30 12:29:16 -0600

    Move class

---
+++ Fire and Ice/Creeper/Move.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Creeper
+{
+    public class Move
+    {
+        public Position StartPosition { get; set; }
+        public Position EndPosition { get; set; }
+        public CreeperColor PlayerColor { get; set; }
+
+        public Move(Position startPosition, Position endPosition, CreeperColor playerColor)
+        {
+            StartPosition = startPosition;
+            EndPosition = endPosition;
+            PlayerColor = playerColor;
+        }
+    }
+}
