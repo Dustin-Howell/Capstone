4c15b2f17038033bd80ab0f3c5416edb16a2aaf4
Dustin Howell <dustin.l.howell@gmail.com>
2013-01-30 15:01:12 -0600

    Moved Position class to its own file.

---
+++ Fire and Ice/Creeper/Position.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Creeper
+{
+    public class Position
+    {
+        public Position() { }
+
+        public Position(int row, int col)
+        {
+            Column = col;
+            Row = row;
+        }
+
+        public int Column { get; set; }
+        public int Row { get; set; }
+
+        public bool Equals(Position position)
+        {
+            return (Column == position.Column && Row == position.Row);
+        }
+    }
+}
