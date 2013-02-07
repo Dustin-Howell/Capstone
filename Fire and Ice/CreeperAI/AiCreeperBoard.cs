cc186bb07c78ac823d1b3cd1abb32a6866f78d09
Kaleb Lape <pingortle@gmail.com>
2013-02-07 13:57:04 -0600

    New CreeperBoard for AI use.

---
+++ Fire and Ice/CreeperAI/AiCreeperBoard.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Creeper;
+using System.Threading;
+using System.Diagnostics;
+using System.IO;
+
+namespace CreeperAI
+{
+    public class AICreeperBoard
+    {
+
+
+        public AICreeperBoard(CreeperBoard board)
+        {
+
+        }
+    }
+}
