9871db77828c41b0151857045de4d3c9dfd08183
Jon Scott Smith <jsmith6503@gmail.com>
2013-02-06 21:34:58 -0600

    Final fix for foolish comment mistakes. Game1.cs is now fully updated.

--- Fire and Ice/XNAControlGame/XNAControlGame/Game1.cs
+++ Fire and Ice/XNAControlGame/XNAControlGame/Game1.cs
@@ -82,6 +82,11 @@ namespace XNAControlGame
             }
         }
 
+        private void drawSelectionBounds(Rectangle currentPostionBounds, int thicknessOfBorder, Color borderColor)
+        {
+
+        }
+
         Move move;
         
         protected override void Initialize()
@@ -166,12 +171,6 @@ namespace XNAControlGame
 
             previousMouseState = mouseState;
 
-            //Peg Selection Logic (Start Position is Selection, End Position is Selection's Move)
-            //A peg (start position) is selected when the user in a turn selects a peg the same color as his turn. This peg is then set as the start position.
-            //The move (end position) is selected when a user selects a valid position that is empty.
-            //If a user selects an invalid position (off board or corner), an invalid end position (invalid move), or the opposite color's peg, the selected peg (start
-            //  position) is deselected (set back to (-1, -1)).
-            //If a user selects a different peg the same color as his turn after already selecting a peg, the selection (start position) is changed to the new peg selection.
             if (start.Row != -1 && end.Row != -1)
             {
                 //last paramiter needs to be changed for sake of change
