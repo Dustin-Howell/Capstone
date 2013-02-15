a71e89d9b323f8f04fb06efbf58226c8aab9ab18
Kaleb Lape <pingortle@gmail.com>
2013-02-14 20:13:19 -0600

    Stub out a view model for the creeper board.

---
+++ Fire and Ice/XNAControlGame/XNAControlGame/CreeperBoardViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Creeper;
+
+namespace XNAControlGame
+{
+    class CreeperBoardViewModel
+    {
+        public CreeperBoard Board { get; private set; }
+
+        // This should map abstract piece positions to spatial positions via array indices.
+        public Position[,] GraphicalPositions { get; private set; }
+
+        CreeperBoardViewModel(CreeperBoard board)
+        {
+            throw new NotImplementedException(this.ToString());
+        }
+    }
+}
