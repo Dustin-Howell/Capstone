30dabe8e82b8075b2e1ad5ecbd8ea342b40462f6
Kaleb Lape <pingortle@gmail.com>
2013-02-15 16:03:17 -0600

    Implement INotifyPropertyChanged on board.

--- Fire and Ice/XNAControlGame/XNAControlGame/CreeperBoardViewModel.cs
+++ Fire and Ice/XNAControlGame/XNAControlGame/CreeperBoardViewModel.cs
@@ -3,19 +3,47 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Creeper;
+using System.ComponentModel;
 
 namespace XNAControlGame
 {
-    class CreeperBoardViewModel
+    class CreeperBoardViewModel : INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler PropertyChanged;
+
         public CreeperBoard Board { get; private set; }
 
         // This should map abstract piece positions to spatial positions via array indices.
         public Position[,] GraphicalPositions { get; private set; }
 
+        public Piece SelectedPiece
+        {
+            get
+            {
+                return _selectedPiece;
+            }
+            set
+            {
+                _selectedPiece = value;
+                OnPropertyChanged("SelectedPiece");
+            }
+        }
+
         CreeperBoardViewModel(CreeperBoard board)
         {
-            throw new NotImplementedException(this.ToString());
+            Board = board;
+            _selectedPiece = null;
         }
+
+        protected void OnPropertyChanged(string name)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(name));
+            }
+        }
+
+        private Piece _selectedPiece;
     }
 }
