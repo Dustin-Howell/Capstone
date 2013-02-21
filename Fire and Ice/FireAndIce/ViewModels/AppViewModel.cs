261c2f7a93b86a70a5206b40add863373bcc0ceb
Kaleb Lape <pingortle@gmail.com>
2013-02-21 16:31:04 -0600

    Begin implementing MVVM GUI with Caliburn Micro.

---
+++ Fire and Ice/FireAndIce/ViewModels/AppViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Caliburn.Micro;
+
+namespace FireAndIce.ViewModels
+{
+    public class AppViewModel : Conductor<Screen>.Collection.OneActive
+    {
+        public AppViewModel()
+        {
+            ActivateItem(new MainMenuViewModel());
+        }
+    }
+}
---
+++ Fire and Ice/FireAndIce/ViewModels/OptionButtonViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Caliburn.Micro;
+
+namespace FireAndIce.ViewModels
+{
+    public class OptionButtonViewModel : PropertyChangedBase
+    {
+        public System.Action ClickAction { get; set; }
+
+        private string _title;
+        public string Title
+        {
+            get
+            {
+                return _title;
+            }
+            set
+            {
+                _title = value;
+                NotifyOfPropertyChange(() => Title);
+            }
+        }
+
+        public void Click()
+        {
+            ClickAction();
+        }
+    }
+}
