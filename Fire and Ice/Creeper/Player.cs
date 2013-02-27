0411dbf135e1bab923676fce599456bf09d066cc
Dustin Howell <Dustin.l.Howell@gmail.com>
2013-02-27 17:05:18 -0600

    forgot to add files before last commit

---
+++ Fire and Ice/Creeper/Player.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Creeper;
+
+namespace Creeper
+{
+    public enum PlayerType { AI, Human, Network }
+
+    public class Player
+    {
+        public CreeperColor Color { get; private set; }
+        public PlayerType PlayerType { get; private set; }
+
+        public Player(PlayerType playerType, CreeperColor creeperColor)
+        {
+            PlayerType = playerType;
+            Color = creeperColor;
+        }
+    }
+}
