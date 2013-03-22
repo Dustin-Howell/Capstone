957c680548bebaac372b9d60ad2cbea204999506
Dustin Howell <Dustin.l.Howell@gmail.com>
2013-03-22 14:12:19 -0500

    added files for rearchitecting (again)

---
+++ Fire and Ice/CreeperMessages/GameStateMessage.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CreeperMessages
+{
+    enum GameState { End, Quit }
+    public class GameStateMessage
+    {
+        public GameState GameState { get; set; }
+
+        public GameStateMessage(GameState newState)
+        {
+            GameState = newState;
+        }
+    }
+}
---
+++ Fire and Ice/CreeperMessages/StartGameMessage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Creeper;
+
+namespace CreeperMessages
+{
+    public class StartGameMessage
+    {
+        public PlayerType Player1Type { get; set; }
+        public PlayerType Player2Type { get; set; }
+        public AIDifficulty AIDifficulty { get; set; }
+    }
+}
