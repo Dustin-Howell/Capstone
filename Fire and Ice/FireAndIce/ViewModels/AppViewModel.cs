249c860dc084d30e6e1ade35951c937d7405fd73
Dustin Howell <Dustin.l.Howell@gmail.com>
2013-04-03 15:57:45 -0500

    Added ugly game over dialog popup

--- Fire and Ice/FireAndIce/ViewModels/AppViewModel.cs
+++ Fire and Ice/FireAndIce/ViewModels/AppViewModel.cs
@@ -5,13 +5,20 @@ using System.Text;
 using Caliburn.Micro;
 using System.IO;
 using CreeperMessages;
+using System.ComponentModel.Composition;
 
 namespace FireAndIce.ViewModels
 {
-    public class AppViewModel : Conductor<Screen>.Collection.OneActive, IHandle<StartGameMessage>
+    [Export(typeof(AppViewModel))]
+    public class AppViewModel : Conductor<Screen>.Collection.OneActive, IHandle<StartGameMessage>, IHandle<GameOverMessage>
     {
-        public AppViewModel()
+        private readonly IWindowManager _windowManager;
+
+        [ImportingConstructor]
+        public AppViewModel(IWindowManager windowManager)
         {
+            _windowManager = windowManager;
+
             AppModel.AppViewModel = this;
             AppModel.EventAggregator.Subscribe(this);
             ActivateItem(new MainMenuViewModel());
@@ -20,6 +27,11 @@ namespace FireAndIce.ViewModels
         public void Handle(StartGameMessage message)
         {
             ActivateItem(new GameContainerViewModel(message.Settings));
+        }
+
+        public void Handle(GameOverMessage message)
+        {
+            _windowManager.ShowDialog(new GameOverViewModel(message.Winner));
         }
     }
 }
---
+++ Fire and Ice/FireAndIce/ViewModels/GameOverViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Creeper;
+using Caliburn.Micro;
+
+namespace FireAndIce.ViewModels
+{
+    public class GameOverViewModel : PropertyChangedBase
+    {
+        private String _winner;
+        private String Winner
+        {
+            get { return _winner ?? ""; }
+            set
+            {
+                _winner = value;
+                NotifyOfPropertyChange(() => GameOverMessage);
+            }
+        }
+
+        public String GameOverMessage
+        {
+            get
+            {
+                return String.Format("{0} wins!", _winner);
+            }
+        }
+
+        public GameOverViewModel(CreeperColor? winner)
+        {
+            _winner = winner.Value.ToString();
+        }
+    }
+}
