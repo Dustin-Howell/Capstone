a8b5c52d204b33efbab3ddd94ec54cd4150be2d6
unknown <jgriffi6@SI-203-02.harding.edu>
2013-04-04 16:01:08 -0500

    Added files for sound stuff.

---
+++ Fire and Ice/CreeperMessages/SoundPlayMessage.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CreeperMessages
+{
+    public enum SoundPlayType {Default }
+
+    public class SoundPlayMessage
+    {
+        public SoundPlayType Type { get; set; }
+
+        public SoundPlayMessage(SoundPlayType type)
+        {
+            Type = type;
+        }
+    }
+}
---
+++ Fire and Ice/CreeperSound/SoundEngine.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using Caliburn.Micro;
+using CreeperMessages;
+using System.Media;
+
+namespace CreeperSound
+{
+    
+    public class SoundEngine : IHandle<SoundPlayMessage>
+    {
+        public SoundEngine(IEventAggregator eventAggregator)
+        {
+            eventAggregator.Subscribe(this);
+        }
+
+
+        public void Handle(SoundPlayMessage message)
+        {
+            String path = Path.GetFullPath("..\\..\\..\\CreeperSound\\SoundAssets");
+            SoundPlayer player = new SoundPlayer(path + "\\default.wav");
+
+            player.Play();
+        }
+    }
+}
