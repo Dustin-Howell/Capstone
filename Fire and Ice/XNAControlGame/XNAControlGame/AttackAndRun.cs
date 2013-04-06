ce753fd9b302ec53252c62a7d0d46e5fad215c27
Jon Scott Smith <ggwaltne@harding.edu>
2013-04-06 17:09:30 -0500

    Added files for the controllers and animation.

---
+++ Fire and Ice/XNAControlGame/XNAControlGame/AttackAndRun.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Nine;
+using Nine.Animations;
+using Nine.Graphics;
+
+namespace XNAControlGame
+{
+    class AttackAndRun : Component
+    {
+        protected override void OnAdded(Group parent)
+        {
+            var model = parent.Find<Nine.Graphics.Model>();
+            var attack = new BoneAnimationController(model.Source.GetAnimation("Attack"));
+            var run = new BoneAnimationController(model.Source.GetAnimation("Run"));
+            run.Speed = 0.8f;
+
+            var blended = new BoneAnimation(model.Skeleton);
+            blended.Controllers.Add(run);
+            blended.Controllers.Add(attack);
+
+            blended.Controllers[run].Disable("Bip01_Pelvis", false);
+            blended.Controllers[run].Disable("Bip01_Spine1", true);
+
+            blended.Controllers[attack].Disable("Bip01", false);
+            blended.Controllers[attack].Disable("Bip01_Spine", false);
+            blended.Controllers[attack].Disable("Bip01_L_Thigh", true);
+            blended.Controllers[attack].Disable("Bip01_R_Thigh", true);
+
+            blended.KeyController = run;
+            blended.IsSychronized = true;
+
+            model.Animations.Play(blended);
+        }
+    }
+}
