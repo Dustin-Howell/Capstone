1685fa358f6a50cf77abc2552578c0971466bd5a
Dustin Howell <Dustin.l.Howell@gmail.com>
2013-04-06 14:31:32 -0500

    my creeperTeacher progress so far

--- Fire and Ice/CreeperCore/CreeperTeacher.cs
+++ Fire and Ice/CreeperCore/CreeperTeacher.cs
@@ -2,10 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Caliburn.Micro;
+using CreeperMessages;
 
 namespace CreeperCore
 {
-    public class CreeperTeacher
+    public class CreeperTeacher : IHandle<MoveMessage>
     {
+
+        public void Handle(MoveMessage message)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
