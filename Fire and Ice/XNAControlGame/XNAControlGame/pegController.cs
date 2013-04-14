24856935cdd1a320a8300fd1e26ddcef0119b5fe
Jon Scott Smith <jsmith6503@gmail.com>
2013-04-13 22:59:57 -0500

    Heavily added to and revised Pegcontroller logic.

--- Fire and Ice/XNAControlGame/XNAControlGame/pegController.cs
+++ Fire and Ice/XNAControlGame/XNAControlGame/pegController.cs
@@ -10,16 +10,32 @@ using Microsoft.Xna.Framework.Input;
 using Microsoft.Xna.Framework.Graphics;
 using Nine.Graphics;
 
+
 namespace XNAControlGame
 {
-    class pegController : Component
+    static class DirectionToDegrees
     {
-        //Placement of this variable and the GetMoveDirection method is temporary (probably).
-        //In the final version all directions should equal some angle relative the board (North would probably be 45 degrees.
-        enum Direction { N, S, E, W, NW, SW, NE, SE };
+        public static float North { get { return MathHelper.ToRadians(315); } }
+        public static float South { get { return MathHelper.ToRadians(135); } }
+        public static float East { get { return MathHelper.ToRadians(225); } }
+        public static float West { get { return MathHelper.ToRadians(45); } }
+        public static float NorthEast { get { return MathHelper.ToRadians(270); } }
+        public static float SouthEast { get { return MathHelper.ToRadians(180); } }
+        public static float NorthWest { get { return MathHelper.ToRadians(0); } }
+        public static float SouthWest { get { return MathHelper.ToRadians(90); } }
+    }
 
-        public float Speed { get; set; }
-        public enum Team { Possible, Fire, Ice };
+    class PegController : Component
+    {
+        public Position Position { get; private set; }
+        public CreeperPegType PegType { get; private set; }
+        public Nine.Graphics.Model PegControlled { get { return Parent.Find<Nine.Graphics.Model>(); } }
+
+        public PegController(Position initPosition, CreeperPegType team)
+        {
+            Position = initPosition;
+            PegType = team;
+        }
 
         protected override void Update(float elapsedTime)
         {
@@ -28,7 +44,6 @@ namespace XNAControlGame
             //it was detected we could pause the tween animation moving peg's position, switch to the kill animation (chop?)
             //and then resume the run and position tween animations until the move is completed. On completion of the positional
             //tween animation, switch from run animation to idle animation. Sheesh.
-
             base.Update(elapsedTime);
         }
 
@@ -43,10 +58,11 @@ namespace XNAControlGame
             return selectionRay.Intersects( Parent.ComputeBounds() ).HasValue;
         }
 
-        private void MoveTo(Move moveToAnimate, Action callback)
+        //public void MoveTo(Move moveToAnimate, Action callback)
+        public void MoveTo(Move moveToAnimate)
         {
-            Direction direction = GetDirection( moveToAnimate );
-            //Set the peg's rotation to the appropriate cardinal direction's angle as set in the enum.
+            //Set the peg's rotation to the appropriate cardinal direction of the move.
+            Parent.Transform *= Matrix.CreateRotationY(GetDirection(moveToAnimate));
 
             //Create the positional tween animation.
             TweenAnimation<Matrix> moveAnimation = new TweenAnimation<Matrix>()
@@ -55,30 +71,32 @@ namespace XNAControlGame
                 TargetProperty = "Transform",
                 Duration = TimeSpan.FromSeconds(1),
                 From = Parent.Transform,
-                To = Matrix.CreateScale(Resources.Models.PegScale) * Matrix.CreateRotationY(MathHelper.ToRadians(135))
+                To = Matrix.CreateScale(Resources.Models.PegScale) * Matrix.CreateRotationY(DirectionToDegrees.South)
                         * Matrix.CreateTranslation(CreeperBoardViewModel.GraphicalPositions[moveToAnimate.EndPosition.Row, moveToAnimate.EndPosition.Column]),
                 Curve = Curves.Smooth,
             };
-            moveAnimation.Completed += new EventHandler((s, e) =>
-            {
-                //Position = position;
-                Parent.Animations["Run"].Stop();
-                Parent.Animations.Play("Idle");
-                Parent.Animations.Remove(Resources.AnimationNames.PegMove);
-                callback();
-            }
-                );
+            //moveAnimation.Completed += new EventHandler((s, e) =>
+            //{
+            //    //Position = position;
+            //    Parent.Animations["Run"].Stop();
+            //    Parent.Animations.Play("Idle");
+            //    Parent.Animations.Remove(Resources.AnimationNames.PegMove);
+            //    callback();
+            //}
+            //    );
 
             Parent.Animations.Add(Resources.AnimationNames.PegMove, moveAnimation);
             Parent.Animations.Play(Resources.AnimationNames.PegMove);
             Parent.Animations.Play("Run");
 
+            Position = moveToAnimate.EndPosition;
+
             //Have different methods based on the type of animation it is for easier scripting? (Move, Capture, Jump)
         }
 
-        private Direction GetDirection(Move moveToAnimate)
+        private float GetDirection(Move moveToAnimate)
         {
-            return Direction.S;
+            return DirectionToDegrees.South;
         }
     }
     
