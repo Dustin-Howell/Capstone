9c29f83a426d4e96aac37524d782490911d2257f
Jon Scott Smith <ggwaltne@harding.edu>
2013-04-18 10:33:22 -0500

    Updated the help menu

--- Fire and Ice/FireAndIce/ViewModels/InstructionsViewModel.cs
+++ Fire and Ice/FireAndIce/ViewModels/InstructionsViewModel.cs
@@ -22,9 +22,27 @@ namespace FireAndIce.ViewModels
             {
                 return @"    1) You can only move your people.
     2) They can jump over a tile space to capture that space (it does not matter if the space is already captured).
-    3) They can jump over an opponent's person to capture that piece.
+    3) They can jump over an opponent's person to destroy that piece.
     4) They can move to any adjacent, unoccupied space.
-    5) They can jump over the home base (this will have no effect on the base).";
+    5) They can jump over the home base (this will have no effect on the base).
+    
+
+    ";
+            }
+        }
+        public String EndGameText
+        {
+            get
+            {
+                return @"    1) How to Win
+        -Connect your two corners on the board. (A connection must have adjacent touching tiles from one corner to the other)
+   
+    2) How to Tie
+       - All of one teams pegs are captured.
+       - A team has no move available to them on their turn.
+    
+
+    ";
             }
         }
     }
