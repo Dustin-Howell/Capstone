46355baa27625aad9929eab047eb88930bfe067b
Dustin Howell <Dustin.l.howell@gmail.com>
2013-04-18 20:20:14 -0500

    chat box works correctly

---
+++ Fire and Ice/FireAndIce/Views/GameContainerView.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FireAndIce.Views
+{
+    public partial class GameContainerView
+    {
+        protected override void OnInitialized(EventArgs e)
+        {
+            base.OnInitialized(e);
+
+            ChatMessages.SizeChanged += new System.Windows.SizeChangedEventHandler(ChatMessages_SizeChanged);
+        }
+
+        void ChatMessages_SizeChanged(object sender, System.Windows.SizeChangedEventArgs e)
+        {
+            ChatMessagesScrollviewer.ScrollToEnd();
+        }
+    }
+}
