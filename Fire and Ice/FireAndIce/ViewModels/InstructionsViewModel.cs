41c523f8808d2e42084429ae46c4b5fa11faa978
Jon Scott Smith <jsmith6503@gmail.com>
2013-04-20 00:21:38 -0500

    Wrote new help text.

--- Fire and Ice/FireAndIce/ViewModels/InstructionsViewModel.cs
+++ Fire and Ice/FireAndIce/ViewModels/InstructionsViewModel.cs
@@ -12,7 +12,7 @@ namespace FireAndIce.ViewModels
         {
             get
             {
-                return "The goal of Creeper is to connect your two corners of the board.";
+                return "\tThe goal of Creeper is to connect your team's corners which are diagonally opposite eachother.";
             }
         }
 
@@ -20,13 +20,22 @@ namespace FireAndIce.ViewModels
         {
             get
             {
-                return @"    1) You can only move your people.
-    2) They can jump over a tile space to capture that space (it does not matter if the space is already captured).
-    3) They can jump over an opponent's person to destroy that piece.
-    4) They can move to any adjacent, unoccupied space.
-    5) They can jump over the home base (this will have no effect on the base).
-    
-
+                return @"    Moving:
+        >There are 3 types of moves your characters can make.
+            1: Neutral
+                -A neutral move is made between the squares of the board.
+                -They do not have any effect on the board and are only used to repostion your characters.
+                -Note: You cannot move to the far corner positions of the board.
+            2: Capture
+                -A capture move is made by jumping diagonally over a square on the board.
+                -This move captures the square for your team and the square will change it's appearance accordingly.
+            3: Attack
+                -An attack move is made much like a neutral move.
+                -When one of your characters is directly adjacent to a character from the opposing team, you can jump
+                    over the opposing character and the character jumped will be removed from play.
+                -You cannot attack a character if the position you must jump to in order to jump over the opposing
+                    character is occupied.
+                -You cannot jump your own team's characters.
     ";
             }
         }
@@ -34,14 +43,13 @@ namespace FireAndIce.ViewModels
         {
             get
             {
-                return @"    1) How to Win
-        -Connect your two corners on the board. (A connection must have adjacent touching tiles from one corner to the other)
-   
-    2) How to Tie
-       - All of one teams pegs are captured.
-       - A team has no move available to them on their turn.
-    
+                return @"    Winning:
+        >There is only one way to win.
+        >You must connect your teams corners through adjacently capture squares.
+        >Note: diagonally adjacent tiles are not considered connected!
 
+    Tie:
+        >The game is considered a draw if either team loses all thier characters.
     ";
             }
         }
